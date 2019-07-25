d1297945bd53df499a5d9d312312c5e78a4f1304
Sungil Saul Lee <jangsoo86@gmail.com>
2019-07-25 01:49:35 +0000

    email editting

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,6 +17,6 @@ $body = "You have received a new message from your website contact form.\n\n"."H
 $header = "From: noreply@domain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+mail($to, $subject, $body, $header);
+return true;
 ?>
